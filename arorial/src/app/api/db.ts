fafb06431004dee68b6911d628d6b1359b0fe5f5
Collin <cdchamberlin.rva@gmail.com>
2024-06-22 12:17:21 -0400

    Fixed minor spelling error.

--- arorial/src/app/api/db.ts
+++ arorial/src/app/api/db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
-    password: process.env.DB_PASWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     dialect: "mysql",
     dialectModule: require("mysql2"),
