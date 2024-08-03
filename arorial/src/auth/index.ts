3d18af8bb99ad98faf14c74d646e7867c19f334c
Collin <cdchamberlin.rva@gmail.com>
2024-08-03 11:42:08 -0400

    Finished login feature. Creaded Database schema for user and account information. Set up Prisma for MySQL database.

--- arorial/src/auth/index.ts
+++ arorial/src/auth/index.ts
@@ -1,70 +1,56 @@
-import SequelizeAdapter, { models } from "@auth/sequelize-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { NextAuthConfig, User } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { DataTypes, Sequelize } from "sequelize";
+import { isValidPassword } from "utils/hash";
+import prisma from "../../lib/prisma";
 
 export const BASE_PATH = "/api/auth";
 
-const sequelize = new Sequelize({
-    host: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    dialect: "mysql",
-    dialectModule: require("mysql2"),
-    database: process.env.DB_NAME,
-    benchmark: true,
-});
 const authOptions: NextAuthConfig = {
     pages: {
         signIn: "/login",
+    },
+    session: {
+        strategy: "jwt",
     },
     providers: [
         Credentials({
             name: "Credentials",
             credentials: {
-                username: {
-                    label: "Username",
-                    type: "text",
-                    placeholder: "jsmith",
+                email: {
+                    label: "Email",
+                    type: "string",
+                    placeholder: "jsmith@example.com",
                 },
                 password: { label: "Password", type: "password" },
             },
-            async authorize(credentials): Promise<User | null> {
-                const users = [
-                    {
-                        id: "test-user-1",
-                        userName: "test1",
-                        name: "Test 1",
-                        password: "pass",
-                        email: "test1@donotreply.com",
-                    },
-                    {
-                        id: "test-user-2",
-                        userName: "test2",
-                        name: "Test 2",
-                        password: "pass",
-                        email: "test2@donotreply.com",
-                    },
-                ];
-                const user = users.find(
-                    (user) =>
-                        user.userName === credentials.username &&
-                        user.password === credentials.password
+            async authorize(credentials: any): Promise<User | null> {
+                if (!credentials?.email || !credentials?.password) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email },
+                });
+
+                if (!user) {
+                    return null;
+                }
+                if (!user.password) {
+                    throw new Error("Please use alternate login method");
+                }
+                const isPasswordMatch = await isValidPassword(
+                    credentials.password,
+                    user.password
                 );
-                return user
-                    ? { id: user.id, name: user.name, email: user.email }
-                    : null;
+
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                };
             },
         }),
     ],
-    adapter: SequelizeAdapter(sequelize, {
-        models: {
-            User: sequelize.define("user", {
-                ...models.User,
-                password: { allowNull: false, type: DataTypes.STRING },
-            }),
-        },
-    }),
+    adapter: PrismaAdapter(prisma),
     basePath: BASE_PATH,
     secret: process.env.AUTH_SECRET,
 };
---
+++ arorial/utils/hash.ts
@@ -0,0 +1,13 @@
+import { compare, hash } from "bcrypt";
+
+export async function hashPassword(password: string) {
+    const hashedPassword = await hash(password, 12);
+    return hashedPassword;
+}
+export async function isValidPassword(
+    password: string,
+    hashedPassword: string
+) {
+    const isValid = await compare(password, hashedPassword);
+    return isValid;
+}
